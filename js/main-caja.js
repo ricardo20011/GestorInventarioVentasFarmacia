cb59ae052b04803a90c0598f6df249ddcf91581f
Ricardo Quebrada <daironquebrada@gmail.com>
2022-11-20 21:33:57 -0500

    Version 4.2 E - Falta corregir errores de reseteo en las filas de venta

--- js/main-caja.js
+++ js/main-caja.js
@@ -19,6 +19,7 @@ tabla = document.getElementById('tabla');
 
 function multiplicarinputs(){
     for(i=1; i < tabla.rows.length; i++){
+        //console.log(i);
         let precio = tabla.rows[i].cells[5].innerHTML;
         precio = quitarSeparadorMiles(precio);
         precio = parseInt(precio);
@@ -26,29 +27,47 @@ function multiplicarinputs(){
         let inputs = tabla.rows[i].cells[6].childNodes;
         
         
-        console.log(inputs[0].setAttribute("id","NV"+[i]));
+        //console.log(inputs[0].setAttribute("id","NV"+[i]));
         
-        function h(){
-            if(inputs[0].value == '' || inputs[0].value == '0' || inputs[0].value < 1){
-                let subprecio = agregarSeparadorMiles(precio);
-                tabla.rows[i].cells[7].innerHTML = subprecio;
-                console.log('se solto1');
-            } else {
-                let total = precio * inputs[0].value ;
-                total = agregarSeparadorMiles(total);
-                
-                tabla.rows[i].cells[7].innerHTML = total;
-            }
+        //function h(){
+        //    if(inputs[0].value == '' || inputs[0].value == '0' || inputs[0].value < 1){
+        //        let subprecio = agregarSeparadorMiles(precio);
+        //        tabla.rows[i].cells[7].innerHTML = subprecio;
+        //        console.log('se solto1');
+        //    } else {
+        //        let total = precio * inputs[0].value ;
+        //        total = agregarSeparadorMiles(total);
+        //        console.log(i);
+        //        tabla.rows[i].cells[7].innerHTML = total;
+        //    }
+        //}
+        
+
+        
+        if(inputs[0].value == '' || inputs[0].value == '0' || inputs[0].value < 1){
+            let subprecio = agregarSeparadorMiles(precio);
+            tabla.rows[i].cells[7].innerHTML = subprecio;
+            console.log('se solto1');
+        } else {
+            let total = precio * inputs[0].value ;
+            total = agregarSeparadorMiles(total);
+            //console.log(i);
+            tabla.rows[i].cells[7].innerHTML = total;
         }
         
-        inputs[0].addEventListener('keyup',()=>{
-            h();
-            console.log('se solto');
-        });
         
-        inputs[0].addEventListener('click',()=>{
-            h();
-        });
+    
+        if(inputs[0].value == '' || inputs[0].value == '0' || inputs[0].value < 1){
+            let subprecio = agregarSeparadorMiles(precio);
+            tabla.rows[i].cells[7].innerHTML = subprecio;
+            console.log('se solto1');
+        } else {
+            let total = precio * inputs[0].value ;
+            total = agregarSeparadorMiles(total);
+            //console.log(i);
+            tabla.rows[i].cells[7].innerHTML = total;
+        }
+
         inputs[0].addEventListener('blur',()=>{
             if(inputs[0].value == '' || inputs[0].value == '0' || inputs[0].value < 1){
                 inputs[0].value = '1';
@@ -60,6 +79,13 @@ function multiplicarinputs(){
     }
 }
 
+
+inputs[0].addEventListener('keyup',()=>{
+    multiplicarinputs();
+});
+inputs[0].addEventListener('click',()=>{
+    multiplicarinputs();
+});
 
 
 
