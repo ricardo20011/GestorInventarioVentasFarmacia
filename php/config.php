954a4e3c345c44fb0f58f61a9059b3afc6e7bba7
ricardo20011 <38635614+ricardo20011@users.noreply.github.com>
2023-02-01 16:51:23 -0500

    Configurando confing EXAMPLE

--- php/config.php
+++ php/config.php
@@ -1,38 +1,33 @@
 <?php 
 
-
+error_reporting(0); //No me muestra errores
 
 // Parametros a configurar para la conexion de la base de datos 
-$host = "162.241.60.251";    // sera el valor de nuestra BD 
-$basededatos = "rickbrok_farmacia";    // sera el valor de nuestra BD 
-$usuariodb = "rickbrok_main";    // sera el valor de nuestra BD 
-$clavedb = "1006690431";    // sera el valor de nuestra BD 
+$host = "162.241.60.251";    // Host o localhost
+$basededatos = "rickbrok_farmacia";    // Nombre base de datos
+$usuariodb = "rickbrok_main";    // Usuario para la base de datos
+$clavedb = "1006690431";    // Clave para la base de datos
 
-//Lista de Tablas
-$tabla_db1 = "usuarios"; 	   // tabla de usuarios
-$tabla_db2 = "ventas";
-$tabla_db3 = "temp";
+//Lista de Tablas de bases de datos
+$tabla_db1 = "usuarios"; 	   // Tabla donde se guarda el inventario
+$tabla_db2 = "ventas";       //Tabla donde se guarda el registro de las ventas
+$tabla_db3 = "temp";      //Tabla donde se validan los usurios en login.php
 
 
-// Seguridad para las entradas
 
-// establecer ruta de del hosting
+// Se crean dos rutas, una cuando se trabaja de manera local, y otra cuando se sube al Hosting
 $local = 'http://localhost/farmacia/';
 $public = 'https://www.rickbroken.com/farmacia/';
-
-
+// Aqui se declara con que ruta vamos a trabajar
 $ruta = $public;
 
-//error_reporting(0); //No me muestra errores
-
+//Se declara la variable $Conexion, la variable se utiliza en los archivos que querramos abrir conexion con las bases
 $conexion = new mysqli($host,$usuariodb,$clavedb,$basededatos);
 
-
+//Mostrar error si hay fallo en la conexion
 if ($conexion->connect_errno) {
     echo "Nuestro sitio experimenta fallos....";
     exit();
 }
-
-
 
 ?>
