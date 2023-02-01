9876505a6cca622ba15cf0750610d0a177c9b2dc
ricardo20011 <38635614+ricardo20011@users.noreply.github.com>
2023-02-01 17:26:35 -0500

    Update config.php

--- php/config.php
+++ php/config.php
@@ -3,10 +3,10 @@
 error_reporting(0); //No me muestra errores
 
 // Parametros a configurar para la conexion de la base de datos 
-$host = "162.241.60.251";    // Host o localhost
-$basededatos = "rickbrok_farmacia";    // Nombre base de datos
-$usuariodb = "rickbrok_main";    // Usuario para la base de datos
-$clavedb = "1006690431";    // Clave para la base de datos
+$host = "";    // Host o localhost
+$basededatos = "";    // Nombre base de datos
+$usuariodb = "";    // Usuario para la base de datos
+$clavedb = "";    // Clave para la base de datos
 
 //Lista de Tablas de bases de datos
 $tabla_db1 = "usuarios"; 	   // Tabla donde se guarda el inventario
@@ -17,7 +17,7 @@ $tabla_db3 = "temp";      //Tabla donde se validan los usurios en login.php
 
 // Se crean dos rutas, una cuando se trabaja de manera local, y otra cuando se sube al Hosting
 $local = 'http://localhost/farmacia/';
-$public = 'https://www.rickbroken.com/farmacia/';
+$public = 'https://www.example.com/';
 // Aqui se declara con que ruta vamos a trabajar
 $ruta = $public;
 
